da1a3196c89ab436630abd446717163218ec0383
David Bodeang <david.bodeang@gmail.com>
2025-11-18 10:59:53 +0100

    Start är klart för vidare

--- Vinterproject/Program.cs
+++ Vinterproject/Program.cs
@@ -14,3 +14,31 @@
 
 // Slut
 // Om spelaren kommer fram till sin plats utan att dö kommer hen vinna spelet men om hen dör så kommer hen förlora spelet
+
+
+
+// Start
+using System.ComponentModel.DataAnnotations;
+
+Console.WriteLine("[===--Elarion--===]");
+Console.WriteLine("--Det har varit ett brutalt inbördeskrig i Elarion, Där kung Alvareon Lysandrel blev avsatt tronen av rebelen Arthur Stark och straffad för sina gryma handlingar mot folket av Elarion--");
+Console.WriteLine("Tryck en valfri knapp för att gå vidare");
+Console.ReadLine();
+Console.Clear();
+
+Console.WriteLine("Välkommen kära spelare, välj ditt namn på din kämpe (max 10 bokstäver)");
+string heroName = Console.ReadLine();
+bool success = int.TryParse(heroName, out int name);
+while (heroName.Length >= 10)
+{
+    Console.WriteLine("Ditt namn får max vara 10 bokstäver");
+    heroName = Console.ReadLine();
+    int.TryParse(heroName, out name);
+}
+
+Console.WriteLine($"Vilket fint namn {heroName}");
+Console.ReadLine();
+Console.Clear();
+
+
+//Beslutagande
