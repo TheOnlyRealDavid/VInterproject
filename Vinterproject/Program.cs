2e363209698176af50c3bcbcda5b448ccac39b88
David Bodeang <david.bodeang@gmail.com>
2025-11-18 10:14:40 +0100

    first commit

---
+++ Vinterproject/Program.cs
@@ -0,0 +1,16 @@
+﻿// Start
+// spelaren presenteras till spelet med en kortfattad förklaring om vad spelet är men en titel, kort historia, instruktioner.
+// Spelaren får välja sitt namn på sin karaktär
+// Spelaren får välja mellan normal eller hardcore mode (skillnaden är antalet guld spelaren börjar med)
+
+// Beslutstagande
+// Spelaren presenteras i medeltidiksa smedja och från välja mellan att köpa 3 olika vapen
+// Spelaren får välja mellan 2 platser som hen vill börja vandra till
+
+// Spelomgången
+// Spelaren kommer bemöta olika händelser på vägen emot den valda platsen
+// När spelaren bemöter en händelse kommer hen få några alternativ hur hen kan lösa dem
+// Beroende på hens val så kommer spelaren antligen förlora något som exempelvis liv, pengar eller få något som exepelvis pengar
+
+// Slut
+// Om spelaren kommer fram till sin plats utan att dö kommer hen vinna spelet men om hen dör så kommer hen förlora spelet
